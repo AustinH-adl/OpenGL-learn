80cc06519e3e5b6a9cb4434f934a2fbb9f085a3d
AustinH-adl <austihh1@gmail.com>
2025-01-14 17:24:54 +1030

    Feat: shader's and shader compiler

--- src/Application.cpp
+++ src/Application.cpp
@@ -1,6 +1,13 @@
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
+
 #include <iostream>
+
+#include "shaderProgram.h"
+
+const char* vertexShaderSource = "C:\\Dev\\OpenGL-learn\\src\\shaders\\shader.vs";
+
+const char* fragmentShaderSource = "C:\\Dev\\OpenGL-learn\\src\\shaders\\shader.fs";
 
 /* Function to resize the viewport given a window and the new size*/
 void framebuffer_size_callback(GLFWwindow* window, int width, int height)
@@ -13,7 +20,6 @@ void processInput(GLFWwindow* window) {
     if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
         glfwSetWindowShouldClose(window, true);
 }
-
 
 int main()
 {
@@ -39,10 +45,28 @@ int main()
     glfwSetFramebufferSizeCallback(window, framebuffer_size_callback); 
 
     float vertices[] = {
-    -0.5f, -0.5f, 0.0f,
-     0.5f, -0.5f, 0.0f,
-     0.0f,  0.5f, 0.0f
+     0.5f,  0.5f, 0.0f, 1.0f, 0.0f, 0.0f,
+     0.5f, -0.5f, 0.0f, 1.0f, 0.0f, 0.0f,
+    -0.5f, -0.5f, 0.0f, 1.0f, 0.0f, 0.0f,
+    -0.5f,  0.5f, 0.0f, 1.0f, 0.0f, 0.0f
     };
+    unsigned int indices[] = {
+        0, 1, 3,
+        1, 2, 3
+    };
+
+    // Creating a VAO (Virtex array object and binding 
+    unsigned int VAO;
+    glGenVertexArrays(1, &VAO);
+    glBindVertexArray(VAO);
+
+    // Creating Element buffer object
+    unsigned int EBO;
+    glGenBuffers(1, &EBO);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+
+    // Passing virtices of Indices array into EBO
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
 
     // Creating a buffer with ID VBO (Virtex buffer object) and binding as an array buffer
     unsigned int VBO;
@@ -51,19 +75,31 @@ int main()
 
     // Passing vertices into the GL_ARRAY_BUFFER in this case VBO
     glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+    
+    ShaderProgram shaderProgram(vertexShaderSource, fragmentShaderSource);
 
+    // Position, Size of array, data type, Normalized, Stride (how many byted between attributes, Offset of data
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)0);
+    glEnableVertexAttribArray(0);
 
-
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)(3 * sizeof(float)));
+    glEnableVertexAttribArray(1);
 
     /* Render loop */
     while (!glfwWindowShouldClose(window))
     {
         processInput(window);
 
+        // Clear background to specified color
         glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
 
+        shaderProgram.use();
 
+        // Drawing 2 triangles from indices 1-6
+        // Draw elements uses EBO   
+        glBindVertexArray(VAO);
+        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
 
         glfwSwapBuffers(window);
         glfwPollEvents();
