1019ffcae14dbaadd27f786d4c60474eb56f3e7a
AustinH-adl <a1923059@adelaide.edu.au>
2025-01-19 15:33:44 +1030

    Adding Texture support and image loading

--- src/Application.cpp
+++ src/Application.cpp
@@ -5,7 +5,8 @@
 #include <filesystem>
 #include <string>
 
-#include "shaderProgram.h"
+#include "headers/shaderProgram.h"
+#include "headers/stb_image.h"
 
 /* Function to resize the viewport given a window and the new size*/
 void framebuffer_size_callback(GLFWwindow* window, int width, int height)
@@ -23,9 +24,6 @@ std::string FileSource(std::string path) {
     std::string dirStr = std::filesystem::current_path().string();
     std::string absPath = dirStr + path;
     return absPath;
-    
-
-
 };
 
 // Path for vertex and fragment shader
@@ -55,49 +53,73 @@ int main()
     }
     glfwSetFramebufferSizeCallback(window, framebuffer_size_callback); 
 
-
-    std::cout << fragmentShaderSource << std::endl;
-
     float vertices[] = {
-     0.5f,  0.5f, 0.0f, 1.0f, 0.0f, 0.0f,
-     0.5f, -0.5f, 0.0f, 1.0f, 0.0f, 0.0f,
-    -0.5f, -0.5f, 0.0f, 1.0f, 0.0f, 0.0f,
-    -0.5f,  0.5f, 0.0f, 1.0f, 0.0f, 0.0f
+        // positions          // colors           // texture coords
+         0.5f,  0.5f, 0.0f,   1.0f, 0.0f, 0.0f,   1.0f, 1.0f,   // top right
+         0.5f, -0.5f, 0.0f,   0.0f, 1.0f, 0.0f,   1.0f, 0.0f,   // bottom right
+        -0.5f, -0.5f, 0.0f,   0.0f, 0.0f, 1.0f,   0.0f, 0.0f,   // bottom left
+        -0.5f,  0.5f, 0.0f,   1.0f, 1.0f, 0.0f,   0.0f, 1.0f    // top left 
     };
+
     unsigned int indices[] = {
         0, 1, 3,
         1, 2, 3
     };
 
-    // Creating a VAO (Virtex array object and binding 
-    unsigned int VAO;
+    // Generating Virtext buffer object, virtex array object and Element buffer object
+    unsigned int VBO, VAO, EBO;
     glGenVertexArrays(1, &VAO);
+    glGenBuffers(1, &VBO);
+    glGenBuffers(1, &EBO);
+
+
     glBindVertexArray(VAO);
 
-    // Creating Element buffer object
-    unsigned int EBO;
-    glGenBuffers(1, &EBO);
-    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+    // Passing vertices into the GL_ARRAY_BUFFER in this case VBO
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
     // Passing virtices of Indices array into EBO
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
     glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
 
-    // Creating a buffer with ID VBO (Virtex buffer object) and binding as an array buffer
-    unsigned int VBO;
-    glGenBuffers(1, &VBO);
-    glBindBuffer(GL_ARRAY_BUFFER, VBO); 
 
-    // Passing vertices into the GL_ARRAY_BUFFER in this case VBO
-    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+    // Arguments: Attribute position, size of attribute in bytes, data type, Normalised data, Stride (space between elements), offset from start of buffer
+
+    // position attribute
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)0);
+    glEnableVertexAttribArray(0);
+    // color attribute
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(3 * sizeof(float)));
+    glEnableVertexAttribArray(1);
+    // texture coord attribute
+    glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(6 * sizeof(float)));
+    glEnableVertexAttribArray(2);
+
+    // Creating and binding texture 
+    unsigned int texture;
+    glGenTextures(1, &texture);
+    glBindTexture(GL_TEXTURE_2D, texture);
+    // Setting parameters for openGL
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+    // Loading image data
+    int width, height, nrChannels;
+    unsigned char* data = stbi_load(FileSource("\\assets\\wall.jpg").c_str(), &width, &height, &nrChannels, 0);
+    if (data) {
+        // Generating texture with loaded asset
+        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
+        glGenerateMipmap(GL_TEXTURE_2D);
+    }
+    else {
+        std::cout << "Failed to load texture" << std::endl;
+    }
+    // Free image data after texture is generated
+    stbi_image_free(data);
     
     ShaderProgram shaderProgram(vertexShaderSource, fragmentShaderSource);
-
-    // Position, Size of array, data type, Normalized, Stride (how many byted between attributes, Offset of data
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)0);
-    glEnableVertexAttribArray(0);
-
-    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)(3 * sizeof(float)));
-    glEnableVertexAttribArray(1);
 
     /* Render loop */
     while (!glfwWindowShouldClose(window))
@@ -111,13 +133,22 @@ int main()
         shaderProgram.use();
 
         // Drawing 2 triangles from indices 1-6
-        // Draw elements uses EBO   
+        // Draw elements uses EBO
+        glBindTexture(GL_TEXTURE_2D, texture);
         glBindVertexArray(VAO);
         glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
 
         glfwSwapBuffers(window);
         glfwPollEvents();
     }
+
+    // Deallocation
+    glDeleteVertexArrays(1, &VAO);
+    glDeleteBuffers(1, &VBO);
+    glDeleteBuffers(1, &EBO);
+
+
+
 
     glfwDestroyWindow(window);
     glfwTerminate();
--- src/shaders/shader.frag
+++ src/shaders/shader.frag
@@ -1,10 +1,12 @@
 #version 330 core
-
-in vec3 ourColor;
-
 out vec4 FragColor;
+  
+in vec3 ourColor;
+in vec2 TexCoord;
 
-void main() 
+uniform sampler2D ourTexture;
+
+void main()
 {
-	FragColor = vec4(ourColor, 1.0f);
+    FragColor = texture(ourTexture, TexCoord)* vec4(ourColor, 1.0);
 }
